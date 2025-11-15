28539500f19d8f90c1a96bbd6f7bc4b10b0dd53e
Jack Espinal <jespinal@lion.lmu.edu>
2025-11-14 19:03:51 -0800

    is valid date file commit

---
+++ is_valid_date.py
@@ -0,0 +1,21 @@
+from days_in_month import days_in_month
+
+def is_valid_date(year: int, months: int, day: int) -> bool:
+    if months < 1 or months > 12:
+        return False
+
+    if day < 1 or day > days_in_month(year, months):
+        return False
+    else:
+        return True
+
+
+if __name__ == '__main__':
+    # Test code.
+    print(f'2025-11-10 (True): {is_valid_date(2025, 11, 10)}')
+    print(f'2027-11-32 (False): {is_valid_date(2027, 11, 32)}')
+    print(f'2024-08-10 (True): {is_valid_date(2024, 8, 10)}')
+    print(f'2024-02-29 (True): {is_valid_date(2024, 2, 29)}')
+    print(f'2023-02-29 (False): {is_valid_date(2023, 2, 29)}')
+    print(f'1900-02-29 (False): {is_valid_date(1900, 2, 29)}')
+    print(f'2000-02-29 (True): {is_valid_date(2000, 2, 29)}')
